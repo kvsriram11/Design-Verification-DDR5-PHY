0f48ae5575296e74a43c582596125f872a05913a
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-02-18 12:52:46 -0800

    Refactor transaction class parameters and data types

--- DDR5PHY_MS2_Class/CLASS_TB/ddr5_transaction.sv
+++ DDR5PHY_MS2_Class/CLASS_TB/ddr5_transaction.sv
@@ -1,7 +1,9 @@
-class transaction #(parameter int n = 7;);
-    rand bit [pNUM:_RANK-1:0] dfi_cs_n_pN[4];
+class transaction #(parameter n = 7,
+		    parameter pNUM_RANK = 2,
+		    parameter pDRAM_SIZE = 4);
+    rand bit [pNUM_RANK-1:0] dfi_cs_n_pN[4];
     rand bit [13:0] dfi_address_pN[4];
-    rand bit [mask_w:0] dfi_wrdata_mask_pN[4];
+    rand bit [(pDRAM_SIZE/4)-1:0] dfi_wrdata_mask_pN[4];
     rand bit [n:0] dfi_wrdata_pN[4];
     rand bit dfi_wrdata_en_pN[4];
 
