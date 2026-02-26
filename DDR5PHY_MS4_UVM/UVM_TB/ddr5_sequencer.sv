13843cf4446a4037caebe92189211e7f3a5b5de3
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-02-25 20:41:58 -0800

    Create ddr5_sequencer.sv

---
+++ DDR5PHY_MS4_UVM/UVM_TB/ddr5_sequencer.sv
@@ -0,0 +1,22 @@
+class ddr5_seqr extends uvm_sequencer #(ddr5_packet);
+
+    //--- UVM Factory Registration (Components because this is static throughout the entire simulation.)
+    `uvm_component_utils(ddr5_seqr)
+
+    function new(string name = "ddr5_seqr", uvm_component parent);
+    super.new(name, parent);
+    `uvm_info("SQR: [%0t] Sequencer Class Started", $time)
+    endfunction //new()
+
+    //build and connect phase are optional here 
+    //for constructing and linking testbench components. 
+    //Ensures top-down approach in the testbench 
+
+
+    task <task_name> ();
+
+
+    #10;
+    `uvm_info("SQR: [%0t] Sequencer Class Ended", $time)
+    endtask //automatic
+endclass //ddr5_seqr extends superClass
