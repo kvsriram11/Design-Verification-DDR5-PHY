321302af785ab8586f0d4a561e0ca870555cbdb9
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-02-27 17:01:35 -0800

    Update ddr5_agent.sv

--- DDR5PHY_MS4_UVM/UVM_TB/ddr5_agent.sv
+++ DDR5PHY_MS4_UVM/UVM_TB/ddr5_agent.sv
@@ -1,3 +1,8 @@
+/* Code: DDR5 PHY Interface code
+   Author: 
+   Course: ECE-593 Fundamentals of Pre-Silicon Validation
+*/
+
 class ddr5_agent extends uvm_agent;
 
     //--- UVM Factory Registration (Components because this is static throughout the entire simulation.)
